93c28680824fb97749c5b5521349b905332948c6
MasterAtharvaJ <me.atharvajadhav@gmail.com>
2023-06-30 18:08:17 +0530

    c6

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const encoder = bodyParser.urlencoded();
 require('dotenv').config();
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
 app.use("/assets", express.static("assets"));
 
 const connection = mysql.createConnection({
